bd075d9b3190828c8e77978444d4469b544beb62
x15411762 <kaitlynncandy7@gmail.com>
2017-04-13 16:58:08 +0100

    restarted app

--- app/src/main/java/com/example/x15411762/cocktail_dreams/homeScreen.java
+++ app/src/main/java/com/example/x15411762/cocktail_dreams/homeScreen.java
@@ -2,12 +2,30 @@ package com.example.x15411762.cocktail_dreams;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.ImageButton;
 
 public class homeScreen extends AppCompatActivity {
 
+    ImageButton GoToNewActivity;
+    ImageButton cocktails;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-    }
+
+    GoToNewActivity = (ImageButton)findViewById(R.id.cocktails));
+
+    cocktails.setOnClickListener(new View.OnClickListener() {
+
+        public void OnClick(View v){
+            Intent intent = new Intent(homeScreen.this, cocktailCategory.class);
+            startActivity(intent);
+        }
+    });
 }
+}
+
+
+
