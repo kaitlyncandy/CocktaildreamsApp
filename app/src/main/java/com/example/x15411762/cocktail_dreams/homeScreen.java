e4afbb20233c4d08d9dd44fa3ed9ea7666153e86
x15411762 <kaitlynncandy7@gmail.com>
2017-04-13 15:25:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/x15411762/cocktail_dreams/homeScreen.java
@@ -0,0 +1,13 @@
+package com.example.x15411762.cocktail_dreams;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class homeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
